026c26443a5bee52cc076c7aac9b8e8acb9383ab
Ovisana <chakriyaheng29@gmail.com>
2025-07-30 11:37:32 +0700

    first commit

---
+++ src/main/java/com/springdemo/spingsecuritybasicauth/SpingSecurityBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.spingsecuritybasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingSecurityBasicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingSecurityBasicAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdemo/spingsecuritybasicauth/SpingSecurityBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.spingsecuritybasicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingSecurityBasicAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
